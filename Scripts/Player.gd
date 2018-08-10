74a367fa0da2e83b2cba2afb0cec988a7d0dbee4
IKStreamIvo <ivostreaming@gmail.com>
2018-08-10 11:28:32 +0200

    RotatingBridge

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,11 @@
-extends KinematicBody
+extends RigidBody
 
 export(float) var moveSpeed = 200
 export(NodePath) var CameraPath
 
 var cam
-const GRAVITY = -9.8 * 3
-var onGround = false
+#const GRAVITY = -9.8 * 3
+#var onGround = false
 
 var velocity = Vector3()
 var direction = Vector3()
@@ -29,15 +29,17 @@ func _physics_process(delta):
 	velocity.x = direction.x
 	velocity.z = direction.z
 
-	if is_on_floor():
-		onGround = true
-	else:
-		if !$RayCast.is_colliding():
-			onGround = false
-
-	if onGround and !is_on_floor():
-		move_and_collide(Vector3(0, -1, 0))
-
-	velocity.y += GRAVITY * delta
-
-	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+#	if is_on_floor():
+#		onGround = true
+#	else:
+#		if !$RayCast.is_colliding():
+#			onGround = false
+#
+#	if onGround and !is_on_floor():
+#		move_and_collide(Vector3(0, -1, 0))
+#
+#	velocity.y += GRAVITY * delta
+	
+	#velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+	
+	set_axis_velocity(velocity)
